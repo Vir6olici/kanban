1ef3bc25f1ffd4770b24a3f8136c6d70bf793f4b
Vir6olici <68445529+Vir6olici@users.noreply.github.com>
2023-09-30 14:11:57 +0300

    Add files via upload
    INITIAL COMMIT FRONTEND

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import  Counter  from "./Counter";
+const App=()=>{
+    let x = 3;
+    return (
+    <div>
+    <h1>Create React App from Scratch Using Typescript..!!</h1>
+    <Counter/>
+    </div>
+    )
+}
+export default App;
