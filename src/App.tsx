83374b59c427c1f0b056df7267af5116b485b20c
alexandru-w98 <stoica.alex.w98@gmail.com>
2023-10-01 10:06:47 +0300

    css modules boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./pages/login/login";
-import Board from "./pages/mainPage/board";
-
+import Board from "./pages/board/board";
 
 let router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Login />,
-    },
-    {
-        path: '/board', 
-        element: <Board />
-    }
-])
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/board",
+    element: <Board />,
+  },
+]);
 
-const App: React.FC =()=>{    
-    return (
-        <React.Fragment>
-            <RouterProvider router={router} />
-        </React.Fragment>
-    )
-}
+const App: React.FC = () => {
+  return (
+    <React.Fragment>
+      <RouterProvider router={router} />
+    </React.Fragment>
+  );
+};
 export default App;
---
+++ src/pages/board/board.tsx
@@ -0,0 +1,7 @@
+import styles from "./board.module.css";
+
+const Board = () => {
+  return <div className={styles["test"]}>board</div>;
+};
+
+export default Board;
